19b2a0957ea65ae776d115ab3554e9c5b0543bad
billwk254 <billkihungu51@gmail.com>
2024-01-23 17:03:53 +0300

    Updated code for task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -22,12 +22,18 @@ def log_stats(mongo_collection):
 
     :param mongo_collection: pymongo collection object
     """
-    total_logs = mongo_collection.count_documents({})
-    print(f"{total_logs} logs")
+    print(f"{mongo_collection.estimated_document_count()} logs")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}: {mongo_collection.count_documents({'method': method})}")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    print(f"{mongo_collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    number_of_gets = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{number_of_gets} status check")
+
+
+if __name__ == "__main__":
+    mongo_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_nginx_stats(mongo_collection)
