aae39a98320a11bb390bae10c4c92729bb30976a
billwk254 <billkihungu51@gmail.com>
2024-01-26 16:11:50 +0300

    Changes

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -36,7 +36,3 @@ def get_page(url: str) -> str:
     """
     response = requests.get(url)
     return response.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
