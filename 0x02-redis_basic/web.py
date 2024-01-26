f82338c67aa0f15b6547c84e66f07ea96df989ae
billwk254 <billkihungu51@gmail.com>
2024-01-26 15:55:59 +0300

    Updated code for web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,39 +6,33 @@ Module for retrieving HTML content from a URL and caching it with Redis.
 
 import requests
 import redis
-import time
-from typing import str
+from typing import Callable
+from functools import wraps
 
 
+redis = redis.Redis()
+
+
+def wrap_requests(fn: Callable) -> Callable:
+    """ Wrapper for the get_page function """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
 def get_page(url: str) -> str:
+    """Get url page
     """
-    Retrieve the HTML content of a given URL and cache the result
-
-    Args:
-        url: The URL to retrieve the HTML content from.
-
-    Returns:
-        str: The HTML content of the URL.
-    """
-    # Initialize Redis connection
-    redis_conn = redis.Redis()
-
-    # Track the number of times the URL was accessed
-    count_key = f"count:{url}"
-    redis_conn.incr(count_key)
-
-    # Check if the HTML content is already cached
-    cached_content_key = f"content:{url}"
-    cached_content = redis_conn.get(cached_content_key)
-
-    if cached_content:
-        return cached_content.decode('utf-8')
-
-    # Retrieve the HTML content from the URL
     response = requests.get(url)
-    html_content = response.text
-
-    # Cache the HTML content with an expiration time of 10 seconds
-    redis_conn.setex(cached_content_key, 10, html_content)
-
-    return html_content
+    return response.text
